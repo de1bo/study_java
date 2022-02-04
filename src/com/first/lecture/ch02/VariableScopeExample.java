777b4b9925e8fbe7712c10f38a7898b599ae7640
de1bo <ogsal5765@naver.com>
2022-02-04 16:13:01 +0900

    cb

---
+++ src/com/first/lecture/ch02/VariableScopeExample.java
@@ -0,0 +1,19 @@
+package com.first.lecture.ch02;
+
+public class VariableScopeExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int v1 = 15;
+		
+		if(v1<10) {
+			int v2;
+			v2 = v1 - 10;
+		}
+		// v2가 같은 블럭에 있지 않기 때문에 에러가 발생
+		//int v3 =v1 + v2 +5;
+	}
+
+}
