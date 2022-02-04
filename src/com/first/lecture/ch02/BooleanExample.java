386886bba3544e2ad9d775b4e4bf62d45f161f96
de1bo <ogsal5765@naver.com>
2022-02-04 17:21:26 +0900

    ㅗㅎㄹ

---
+++ src/com/first/lecture/ch02/BooleanExample.java
@@ -0,0 +1,18 @@
+package com.first.lecture.ch02;
+
+public class BooleanExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean stop = true;
+		
+		if(stop) {
+			System.out.println("중지합니다.");
+		} else {
+			System.out.println("실행합니다.");
+		}
+		
+	}
+
+}
