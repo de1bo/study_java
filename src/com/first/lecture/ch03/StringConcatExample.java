38868215066bd5d4a4581902535b0e0348bda7ee
de1bo <ogsal5765@naver.com>
2022-02-10 13:56:25 +0900

    gdf

---
+++ src/com/first/lecture/ch03/StringConcatExample.java
@@ -0,0 +1,14 @@
+package com.first.lecture.ch03;
+
+public class StringConcatExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String str1 = "JDK" + 1.8;
+		String str2 = str1 +"입니다.";
+				
+		System.out.println("str2: " + str2);
+	}
+
+}
