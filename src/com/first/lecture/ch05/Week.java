1ac56a3caa2c07f8bf7a6442fa9d09febb7c9c94
de1bo <ogsal5765@naver.com>
2022-02-16 16:14:18 +0900

    vxvx

---
+++ src/com/first/lecture/ch05/Week.java
@@ -0,0 +1,17 @@
+package com.first.lecture.ch05;
+
+public enum Week {	// Enumberation Type 
+
+	// Enumberaion constants : 열거 상수
+	// 무조건 대문자
+	// 복합문자는 언더바 사용)  BLUE_MONDAY : snake
+	
+	
+	MONDAY,
+	TUESDAY,
+	WEDESDAY,
+	THURSDAY,
+	FRIDAY,
+	SATURDAY,
+	SUNDAY
+}
