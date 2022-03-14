ff4ab7f9895cad0e814710d84fcc5030416a6881
de1bo <ogsal5765@naver.com>
2022-03-14 18:28:47 +0900

    kjgbv

--- src/com/first/lecture/ch13/ItemExample.java
+++ src/com/first/lecture/ch13/ItemExample.java
@@ -5,6 +5,15 @@ public class ItemExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Product<Car, Integer> product = new Product<>();
+		
+		product.setKind(new Car());
+		product.setModel(400);
+		
+		Car rtCar = product.getKind();
+		int rtString = product.getModel();
+		
+		System.out.println(rtCar + " : " + rtString);
 	}
 
 }
