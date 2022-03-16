e03b194624b185380efd75e206cf25b13ab6509f
de1bo <ogsal5765@naver.com>
2022-03-16 16:13:07 +0900

    fghkj

---
+++ src/com/first/lecture/ch13/BoundedTypeParameterExample.java
@@ -0,0 +1,19 @@
+package com.first.lecture.ch13;
+
+public class BoundedTypeParameterExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		String str= Util.compare("a","b");
+		
+		int result1 =Util3.compare(10, 20);
+		System.out.println(result1);
+		
+		int result2 =Util3.compare(4.5, 3);
+		System.out.println(result2);
+		
+		
+	}
+
+}
---
+++ src/com/first/lecture/ch13/Util.java
@@ -0,0 +1,10 @@
+package com.first.lecture.ch13;
+
+public class Util {
+
+		public static<T> Box<T> boxing(T t){
+			Box<T> box = new Box<T>();
+			box.set(t);
+			return box;
+		}
+}
